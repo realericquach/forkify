1cf7b9e84d275d02761b5492bca1e9ef3a60057d
realericquach <realericquach@gmail.com>
2024-05-03 11:17:46 -0500

    bookmarks

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -3,11 +3,14 @@ import icons from 'url:../../img/icons.svg';
 export default class View {
   _data;
 
-  render(data) {
+  render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
     this._data = data;
     const markup = this._generateMarkup();
+
+    if (!render) return markup;
+
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
@@ -27,7 +30,7 @@ export default class View {
       //Updates changed TEXT
       if (
         !newEl.isEqualNode(currEl) &&
-        newEl.firstChild.nodeValue.trim() !== ''
+        newEl.firstChild?.nodeValue.trim() !== ''
       ) {
         currEl.textContent = newEl.textContent;
       }
